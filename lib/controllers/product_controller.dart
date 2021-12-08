64708aa63ffd47e276fc35a620dfd30dbda25a1d
Jungyeon <yeonns2@gmail.com>
2021-12-09 03:01:09 +0900

    feat : 스토어 제품 조회 API 연동

--- lib/controllers/product_controller.dart
+++ lib/controllers/product_controller.dart
@@ -1,8 +1,24 @@
 import 'package:FinDit/models/product.dart';
-import 'package:FinDit/repository/product_service.dart';
+import 'package:FinDit/services/product_service.dart';
 import 'package:get/get.dart';
 
 class ProductController extends GetxController {
+  static ProductController get to => Get.find();
   var isLoading = true.obs;
-  var productList = RxList<Product>().obs;
+  var products = <Product>[].obs;
+  @override
+  void onInit() {
+    loadproducts();
+    super.onInit();
+  }
+
+  void loadproducts() async {
+    try {
+      isLoading(true);
+      var productList = await ProductService.to.fetchAllProduct();
+      products.assignAll(productList);
+    } finally {
+      isLoading(false);
+    }
+  }
 }
